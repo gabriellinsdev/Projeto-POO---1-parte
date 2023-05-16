9e737a080176610e49ed04ce21cf0c241be1ea9a
Gabriel Lins Soares Silvestre <gabriel.lins.silvestre@gmail.com>
2023-05-15 23:57:18 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/mycompany/sistematelefoniajava/Telefonia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistematelefoniajava;
+
+/**
+ *
+ * @author gabri
+ */
+public class Telefonia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
